5542a91bd3a504572ed4507d50260e6a83dd7718
Isaias <isaias.alecio@gmail.com>
2017-09-20 15:46:34 -0600

    version agregada el 19092017

--- app/src/main/java/com/mosi/tdpsync/utils/ValidaEspacio.java
+++ app/src/main/java/com/mosi/tdpsync/utils/ValidaEspacio.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
-import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -43,10 +42,8 @@ public class ValidaEspacio {
 
     static byte[] utf8usuarios,utf8companias,utf8clientes,utf8tipos,utf8invptdtab,utf8invptmtab,utf8pr1tab,utf8talonarios,utf8pedidos;
     public float Adescargar;
-    public long descarga;
+    public int descarga;
     static final DecimalFormat df = new DecimalFormat("0.0");
-
-
 
     public float verificador(final Activity activity){
 
@@ -64,7 +61,6 @@ public class ValidaEspacio {
                  talonarios=httpGetData(talonarios);
                  pedidos=httpGetData(pedidos);
                 try {
-
                     utf8usuarios = usuarios.getBytes();
                     utf8companias = companias.getBytes();
                     utf8clientes = clientes.getBytes();
@@ -75,19 +71,23 @@ public class ValidaEspacio {
                     utf8talonarios = talonarios.getBytes();
                     utf8pedidos = pedidos.getBytes();
 
+                    descarga = usuarios.getBytes().length+companias.getBytes().length+clientes.getBytes().length+tipos.getBytes().length+
+                            invptdtab.getBytes().length+invptmtab.getBytes().length+pr1tab.getBytes().length+talonarios.getBytes().length+pedidos.getBytes().length;
+
                     System.out.println( "espacio "+utf8usuarios.length +" "+utf8companias.length+" "+ utf8clientes.length+" "+utf8tipos.length+" "+utf8invptdtab.length+" "+utf8invptmtab.length
                             +" "+utf8pr1tab.length+" "+utf8talonarios.length);
 
                     Adescargar = utf8usuarios.length+utf8companias.length+utf8clientes.length+utf8tipos.length+
                             utf8invptdtab.length+utf8invptmtab.length+utf8pr1tab.length+utf8talonarios.length;
 
-                    System.out.println("descarga esp Adescargar  "+descarga);
+                    System.out.println("variable descarga " +descarga);
+                    descarga =  (descarga/(1024));
+                    System.out.println("variable descarga convertido a MB " +descarga);
 
                     long MEGABYTE = 1024L*1024L;
                     Adescargar = Adescargar/MEGABYTE;
 
                     System.out.println("Adescargar despues de conversion  "+Adescargar+ " MB" );
-
 
                     activity.runOnUiThread(new Runnable() {
                         @Override
@@ -95,26 +95,21 @@ public class ValidaEspacio {
                             float disponible = getMegabytesAvailable();
                             System.out.println(" disponible "+disponible+" Adescargar "+Adescargar);
                             System.out.println("Adescargar despues de conversion  "+Adescargar+" 2");
-                            if (disponible<Adescargar){
+                            System.out.println("descarga despues de conversion  "+descarga+" 2");
+                            if (disponible<descarga){
                              //  Toast existe = Toast.makeText(activity, "No cuentas con espacio para una sincronizacion", Toast.LENGTH_LONG);
                                // existe.show();
-                                System.out.println("Adescargar menor "+Adescargar);
-                                System.out.println(" disponible "+disponible+" Adescargar "+Adescargar+ " dentro de if negativo");
-
+                                System.out.println("No hay espacio disponible para una sincronizacion tu dispones de "+disponible +"MB y la descarga es de "+descarga + "MB N");
                             }else{
                                 //Toast existe = Toast.makeText(activity, "Cuentas con espacio disponible", Toast.LENGTH_LONG);
                                // existe.show();
-                                System.out.println("Adescargar mayor"+Adescargar);
-                                System.out.println(" disponible "+disponible+" Adescargar "+Adescargar+ " dentro de if positivo");
+                                System.out.println("Cuentas con espacio disponible para una sincronizacion tu dispones de "+disponible +"MB  y la descarga es de "+descarga + "MB P");
                             }
                         }
                     });
-
                 } catch (Exception e) {
                     System.out.println("Error " + e.toString());
-
                     e.printStackTrace();
-
                 }
             }
         });
@@ -173,6 +168,11 @@ public class ValidaEspacio {
         long  MEGABYTE = 1024L * 1024L * 1024L;
         System.out.println("Adescargar dentro de bytetoMeg "+bytes / MEGABYTE );
         return bytes / MEGABYTE ;
+    }
+
+    public void cargaEspacio(){
+        System.out.println("Se esta cargando el espacio disponible en el dispositivo");
+        System.out.println("Finzalizando la carga del espacio disponible en el dispositivo");
     }
 
 }
